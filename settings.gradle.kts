485245f9044543f98321db5b1d44e2121bce67e6
first <markgelorioflorido@gmail.com>
2023-12-06 23:29:15 +0800

    second commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MarkGeloRioflorido-Block2-Quiz"
+include(":app")
+ 
